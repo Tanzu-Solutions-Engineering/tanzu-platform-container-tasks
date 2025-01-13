5bbb6688a17efc01a33f88b86001df2a62804796
warroyo <warroyo7199008@gmail.com>
2025-01-13 10:37:32 -0700

    updates for paths and syntax

--- volume-based-secret-task/src/task.py
+++ volume-based-secret-task/src/task.py
@@ -13,9 +13,9 @@ def get_object_by_property(array, prop, value):
             return obj
     return None
 
-schema_file = 'z-mount-schema.yml'
+schema_file = '/app/z-mount-schema.yml'
 values_file = 'z-mount-values.yml'
-overlay_file = 'z-mount-overlay.yml'
+overlay_file = '/app/z-mount-overlay.yml'
 
 extra_comments = """\
 #@data/values
@@ -24,7 +24,7 @@ extra_comments = """\
 """
 
 workspaceDir = os.environ.get('TANZU_BUILD_WORKSPACE_DIR')
-configDir = f"{workspaceDir}/output/config/"
+configDir = f"{workspaceDir}/package/config/"
 
 print("adding volume mount overlay")
 with open(workspaceDir + '/output/containerapp.yml', 'r') as file:
@@ -37,9 +37,10 @@ if "ext.apps.tanzu.vmware.com/mount-secret-as-file" in containerapp["metadata"][
     for mount in mountsParsed:
         details = mount.split("=")
         name = details[0]
+        k8s_name = name.lower().replace("_","-")
         path = details[1]
         secretRef = get_object_by_property(containerapp['spec']['secretEnv'], "name",name )
-        volume = {'name': name,'secret': secretRef['secretKeyRef']['name'],'path': path,'key': secretRef['secretKeyRef']['key']}
+        volume = {'name': k8s_name,'secret': secretRef['secretKeyRef']['name'],'path': path,'key': secretRef['secretKeyRef']['key'],'varName': name}
         volumes['ext_mount_secret']['volumes'].append(volume)
 
     with open(values_file, "w") as file:
@@ -50,6 +51,8 @@ if "ext.apps.tanzu.vmware.com/mount-secret-as-file" in containerapp["metadata"][
     shutil.copy2(values_file,configDir)
     shutil.copy2(schema_file,configDir)
     shutil.copy2(overlay_file,configDir)
+
+    os.remove(values_file)
 
 
 
